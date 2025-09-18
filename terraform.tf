e7ddd7e7ade8f9aa4fd71dd5e381fd8d192eda47
acornies <andrew.cornies@hashicorp.com>
2025-09-18 14:49:26 -0400

    update both module and provider versions

--- terraform.tf
+++ terraform.tf
@@ -8,9 +8,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.10.0"
+      version = "~> 6.13.0"
     }
   }
-
-  required_version = "~> 1.4.0"
 }
